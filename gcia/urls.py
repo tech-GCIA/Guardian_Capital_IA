089aa231b55b39977ff47b772d95d8f639f8de81
Abhishek <starkabhishek29@gmail.com>
2025-05-12 01:28:24 +0530

    minor update

--- gcia/urls.py
+++ gcia/urls.py
@@ -17,7 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path("app/", include("gcia_app.urls")),
     path('app/admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
