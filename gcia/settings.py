8943b216678f203af58694421af463cb05b34004
Abhishek <starkabhishek29@gmail.com>
2025-05-12 23:33:31 +0530

    minor update

--- gcia/settings.py
+++ gcia/settings.py
@@ -128,9 +128,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'static/'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "gcia_app/static"]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'gcia_app', 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
